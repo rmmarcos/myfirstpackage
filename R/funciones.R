9c981e1ad58541fab74534b35031e90352ba8d71
rmmarcos <r.marcos.1980@gmail.com>
2021-12-30 09:36:35 +0100

    reparacion paquete

--- R/funciones.R
+++ R/funciones.R
@@ -1,9 +1,6 @@
 #' Calculate NDVI
 #'
-#' This function loads a file as a matrix. It assumes that the first column
-#' contains the rownames and the subsequent columns are the sample identifiers.
-#' Any rows with duplicated row names will be dropped with the first one being
-#' kepted.
+#' This function caculates the normalized difference vegetation index.
 #'
 #' @param red value or vector with reflectance in the Red wavelenght
 #' @param nir value or vector with reflectance in the NIR wavelenght
