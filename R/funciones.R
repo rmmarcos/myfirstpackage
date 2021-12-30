9d3a074e25117b9406d82c404715cfda5faacc19
rmmarcos <r.marcos.1980@gmail.com>
2021-12-30 09:31:16 +0100

    Arreglada descripcion de funciones para describir correctamente el resultado

--- R/funciones.R
+++ R/funciones.R
@@ -7,25 +7,11 @@
 #'
 #' @param red value or vector with reflectance in the Red wavelenght
 #' @param nir value or vector with reflectance in the NIR wavelenght
-#' @return A matrix of the infile
+#' @return NDVI value
 #' @export
 
 ndvi <- function(red,nir){
   return((nir-red)/(nir+red))
 }
 
-#' Calculate NDVI
-#'
-#' This function loads a file as a matrix. It assumes that the first column
-#' contains the rownames and the subsequent columns are the sample identifiers.
-#' Any rows with duplicated row names will be dropped with the first one being
-#' kepted.
-#'
-#' @param red value or vector with reflectance in the Red wavelenght
-#' @param nir value or vector with refance in the NIR wavelenght
-#' @return A matrix of the infile
-#' @export
 
-ndvi2 <- function(red,nir){
-  return((nir-red)/(nir+red))
-}
