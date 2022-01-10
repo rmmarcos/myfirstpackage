9866c348cdd3783a02756b1a28216da6438c3502
rmmarcos <r.marcos.1980@gmail.com>
2022-01-10 08:11:22 +0100

    modify function

--- R/funciones.R
+++ R/funciones.R
@@ -2,13 +2,13 @@
 #'
 #' This function caculates the normalized difference vegetation index.
 #'
-#' @param red value or vector with reflectance in the Red wavelenght
-#' @param nir value or vector with reflectance in the NIR wavelenght
+#' @param b1 value or vector with reflectance in the first wavelenght
+#' @param b2 value or vector with reflectance in the second wavelenght
 #' @return NDVI value
 #' @export
 
-ndvi <- function(red,nir){
-  return((nir-red)/(nir+red))
+ndiff <- function(b1,b2){
+  return((b1-b2)/(b1+b2))
 }
 
 
